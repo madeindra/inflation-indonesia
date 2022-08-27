4045f2f50e359cf2f8c64b081b9c06a38c55d7d4
Made Indra <made.indra@protonmail.com>
2022-08-27 17:57:10 +0700

    wip: add calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './App.module.css';
+import data from './data/inflation.json';
 
 function App() {
+  const yearData = Object.keys(data);
+  const rateData = Object.values(data);
+
+  const [currentValue, setCurrentValue] = useState(0);
+  const [predictedValue, setPredictedValue] = useState(0);
+  const [equalValue, setEqualValue] = useState(0);
+  
+  const [formattedCurrentValue, setFormattedCurrent] = useState('0');
+
+  const [from, setFrom] = useState(2011);
+  const [to, setTo] = useState(2022);
+
+  function calculate() {
+    const fromIndex = yearData.findIndex((item) => Number(item) === Number(from));
+    const toIndex = yearData.findIndex((item) => Number(item) === Number(to));
+
+    if (fromIndex === -1 || toIndex === -1){
+      return;
+    }
+
+    let totalInflation = 0;
+
+    if (from < to) {
+      for (let idx = fromIndex; idx <= toIndex; idx++) {
+        totalInflation += rateData[idx];
+      }
+
+      setPredictedValue(Math.floor(currentValue - (currentValue * (totalInflation / 100))));
+      setEqualValue(Math.floor(currentValue + (currentValue * (totalInflation / 100))));
+    }
+
+    if (from > to) {
+      for (let idx = fromIndex; idx >= toIndex; idx--) {
+        totalInflation += rateData[idx];
+      }
+
+      setPredictedValue(Math.floor(currentValue + (currentValue * (totalInflation / 100))));
+      setEqualValue(Math.floor(currentValue - (currentValue * (totalInflation / 100))));
+    }
+  }
+
+  function formatSeparator(text) {
+    return String(text).replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  }
+
+  function stringToNumber(text) {
+    return Number(String(text).replace('.', ''));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={styles.row}>
+        <input
+          className={styles.textbox}
+          onChange={(event) => {
+            setCurrentValue(stringToNumber(event.target.value));
+            setFormattedCurrent(formatSeparator(stringToNumber(event.target.value)))
+          }}
+          value={formattedCurrentValue}
+        />
+      </div>
+      <div className={styles.row}>
+      </div>
+      <div className={styles.row}>
+        <p><strong>{formatSeparator(currentValue)}</strong> pada tahun <strong>{from}</strong> setara dengan <strong>{formatSeparator(predictedValue)}</strong> pada tahun <strong>{to}</strong></p>
+      </div>
+      <div className={styles.row}>
+      <p>Butuh <strong>{formatSeparator(equalValue)}</strong> pada tahun <strong>{to}</strong> agar nilainya sama dengan <strong>{formatSeparator(currentValue)}</strong> pada tahun <strong>{from}</strong></p>
+      </div>
     </div>
   );
 }
